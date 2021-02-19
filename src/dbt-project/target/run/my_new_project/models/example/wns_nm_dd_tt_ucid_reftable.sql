b63e853f6bb88252b1ac43b11f194f2dd9d1b969
Raamy Schroeder <raamyschroeder@woolworths.co.za>
2021-02-19 13:07:20 +0000

    this is for Allims explanation

---
+++ src/dbt-project/target/run/my_new_project/models/example/wns_nm_dd_tt_ucid_reftable.sql
@@ -0,0 +1,7 @@
+
+
+  create view "dev"."development"."wns_nm_dd_tt_ucid_reftable__dbt_tmp" as (
+    SELECT perm_ucid_hashed as ucid, SUBSCRIBER_KEY,CUSTOMER_NO,'CARDS' AS CARD_TYPE
+	FROM DEVELOPMENT.DIM_CUSTOMER_CARD_MAPPING
+	GROUP BY 1,2,3,4
+  ) ;
